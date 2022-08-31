4f3dadd39123d102fda39c46f20ebe910ec7848f
IODevBlue <iodevblue@gmail.com>
2022-08-31 23:44:01 +0100

    Added ColumnBreaker documentation

--- library/src/main/kotlin/com/blueiobase/api/android/chiplayoutmanager/breaker/ColumnBreakerFactory.kt
+++ library/src/main/kotlin/com/blueiobase/api/android/chiplayoutmanager/breaker/ColumnBreakerFactory.kt
@@ -1,7 +1,17 @@
 package com.blueiobase.api.android.chiplayoutmanager.breaker
 
+import android.view.View
 import com.blueiobase.api.android.chiplayoutmanager.breaker.contract.IBreakerFactory
+import com.blueiobase.api.android.chiplayoutmanager.main.ChipsLayoutManager
 
+/**
+ * This class is responsible for breaking the [View] object anchored at the extreme ends of the
+ * [ChipsLayoutManager] when the layout orientation is [vertical][ChipsLayoutManager.VERTICAL].
+ *
+ * @author IO DevBlue
+ * @since 1.0.0
+ *
+ */
 class ColumnBreakerFactory: IBreakerFactory {
 
     override fun createBackwardRowBreaker() = LTRBackwardColumnBreaker()
--- library/src/main/kotlin/com/blueiobase/api/android/chiplayoutmanager/breaker/LTRBackwardColumnBreaker.kt
+++ library/src/main/kotlin/com/blueiobase/api/android/chiplayoutmanager/breaker/LTRBackwardColumnBreaker.kt
@@ -1,8 +1,17 @@
 package com.blueiobase.api.android.chiplayoutmanager.breaker
 
+import android.view.View
 import com.blueiobase.api.android.chiplayoutmanager.layouter.AbstractLayouter
 import com.blueiobase.api.android.chiplayoutmanager.breaker.contract.ILayoutRowBreaker
+import com.blueiobase.api.android.chiplayoutmanager.main.ChipsLayoutManager
 
+/**
+ * This class is responsible for breaking the [View] object anchored at the right/end for the [ColumnBreakerFactory].
+ *
+ * @author IO DevBlue
+ * @since 1.0.0
+ *
+ */
 class LTRBackwardColumnBreaker: ILayoutRowBreaker {
 
     override fun isRowBroke(abstractLayouter: AbstractLayouter): Boolean {
